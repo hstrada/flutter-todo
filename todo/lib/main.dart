8d077f8ac0bf729346e677431d1b9c0ddcf42d03
hstrada <h.strada@hotmail.com>
2021-02-21 18:47:34 -0300

    chore: add sharedpreferences to save local items

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'models/item.dart';
 
@@ -23,9 +26,9 @@ class HomePage extends StatefulWidget {
   var items = new List<Item>();
   HomePage() {
     items = [];
-    items.add(Item(title: "Item 1", done: false));
-    items.add(Item(title: "Item 2", done: true));
-    items.add(Item(title: "Item 3", done: false));
+    // items.add(Item(title: "Item 1", done: false));
+    // items.add(Item(title: "Item 2", done: true));
+    // items.add(Item(title: "Item 3", done: false));
   }
 
   @override
@@ -45,12 +48,38 @@ class _HomePageState extends State<HomePage> {
       ));
       newTaskCtrl.text = "";
     });
+    save();
   }
 
   void remove(int index) {
     setState(() {
       widget.items.removeAt(index);
     });
+    save();
+  }
+
+  // tem que ser assincrona, não é real time
+  Future load() async {
+    // aguarde enquanto estiver carregado
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString("data");
+
+    if (data != null) {
+      Iterable decoded = jsonDecode(data);
+      List<Item> result = decoded.map((x) => Item.fromJson(x)).toList();
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('data', jsonEncode(widget.items));
+  }
+
+  _HomePageState() {
+    load();
   }
 
   @override
@@ -90,6 +119,7 @@ class _HomePageState extends State<HomePage> {
                 setState(() {
                   item.done = value;
                 });
+                save();
               },
             ),
             key: Key(item.title),
